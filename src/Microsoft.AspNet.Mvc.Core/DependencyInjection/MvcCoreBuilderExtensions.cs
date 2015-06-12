3b12e02ac09385208a169c7a143fe8b6c0d3f510
Ryan Nowak <nowakra@gmail.com>
2015-06-12 10:26:09 -0700

    Split MVC into a million pieces

---
+++ src/Microsoft.AspNet.Mvc.Core/DependencyInjection/MvcCoreBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Internal;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class MvcCoreBuilderExtensions
+    {
+        public static IMvcBuilder Configure(
+            [NotNull] this IMvcBuilder builder,
+            [NotNull] Action<MvcOptions> setupAction)
+        {
+            builder.Services.Configure(setupAction);
+            return builder;
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Mvc.Formatters.Json/DependencyInjection/MvcExtensionsBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Internal;
+using Microsoft.Framework.OptionsModel;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class MvcExtensionsBuilderExtensions
+    {
+        public static IMvcBuilder AddJsonFormatters([NotNull] this IMvcBuilder builder)
+        {
+            builder.Services.AddTransient<IConfigureOptions<MvcOptions>, JsonMvcOptionsSetup>();
+            return builder;
+        }
+
+        public static IMvcBuilder AddJsonFormatters(
+            [NotNull] this IMvcBuilder builder,
+            [NotNull] Action<MvcJsonOptions> setupAction)
+        {
+            AddJsonFormatters(builder);
+            ConfigureJsonFormatters(builder, setupAction);
+            return builder;
+        }
+
+        public static IMvcBuilder ConfigureJsonFormatters(
+            [NotNull] this IMvcBuilder builder,
+            [NotNull] Action<MvcJsonOptions> setupAction)
+        {
+            builder.Services.Configure(setupAction);
+            return builder;
+        }
+    }
+}
